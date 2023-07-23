050d07c865674a7bf5f23363b184e8804432129b
@hexfa <amir.dehdarian2009@gmail.com>
2023-07-23 14:12:45 +0100

    First camera testing via getUserMedia

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
 
 function App() {
+  const [selfieImage, setSelfieImage] = useState(null);
   const [count, setCount] = useState(0)
 
+  function manuallyStartCamera() {
+    startCamera();
+    }
+  async function takeSelfie() {
+    const videoElement = document.querySelector('#video-preview');
+  
+    //It's metadata, which includes videoWidth and videoHeight
+    await videoElement.play();
+    await new Promise((resolve) => (videoElement.onloadedmetadata = resolve));
+  
+    const canvas = document.createElement('canvas');
+    canvas.width = videoElement.videoWidth;
+    canvas.height = videoElement.videoHeight;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+  
+    // Get the selfie image data as a base64 string
+    const selfieDataUrl = canvas.toDataURL('image/png');
+  
+    // Update the state with the selfie image
+    setSelfieImage(selfieDataUrl);
+  }  
+
+  async function startCamera() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const videoElement = document.querySelector('#video-preview');
+      videoElement.srcObject = stream;
+    } catch (error) {
+      console.error('Error accessing camera:', error);
+    }
+  }  
+  
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>React Selfie App</h1>
+      <video id="video-preview" autoPlay></video>
+      <button onClick={manuallyStartCamera}>Start Camera</button>
+    </div>
+  );
 }
 
 export default App
