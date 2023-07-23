29e286ff06640f9a6147af2abe93babbdcd8ce98
@hexfa <amir.dehdarian2009@gmail.com>
2023-07-23 14:27:50 +0100

    Added camera mirror feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,58 @@
-import { useState } from 'react'
-import './App.css'
-import React from 'react'
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
   const [selfieImage, setSelfieImage] = useState(null);
-  const [count, setCount] = useState(0)
+  const [mirror, setMirror] = useState(false);
+  const videoRef = useRef(null);
 
-  function manuallyStartCamera() {
+  useEffect(() => {
     startCamera();
-    }
-  async function takeSelfie() {
-    const videoElement = document.querySelector('#video-preview');
-  
-    //It's metadata, which includes videoWidth and videoHeight
-    await videoElement.play();
-    await new Promise((resolve) => (videoElement.onloadedmetadata = resolve));
-  
-    const canvas = document.createElement('canvas');
-    canvas.width = videoElement.videoWidth;
-    canvas.height = videoElement.videoHeight;
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
-  
-    // Get the selfie image data as a base64 string
-    const selfieDataUrl = canvas.toDataURL('image/png');
-  
-    // Update the state with the selfie image
-    setSelfieImage(selfieDataUrl);
-  }  
+  }, []);
 
   async function startCamera() {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-      const videoElement = document.querySelector('#video-preview');
-      videoElement.srcObject = stream;
+      videoRef.current.srcObject = stream;
+      videoRef.current.play(); // Start playing the video
     } catch (error) {
       console.error('Error accessing camera:', error);
     }
-  }  
-  
+  }
+
+  async function takeSelfie() {
+    const canvas = document.createElement('canvas');
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+    const ctx = canvas.getContext('2d');
+    if (mirror) {
+      ctx.translate(canvas.width, 0);
+      ctx.scale(-1, 1);
+    }
+    ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+
+    // Get the selfie image data as a base64 string
+    const selfieDataUrl = canvas.toDataURL('image/png');
+    setSelfieImage(selfieDataUrl);
+  }
+
+  function toggleMirror() {
+    setMirror(!mirror);
+  }
 
   return (
     <div>
       <h1>React Selfie App</h1>
-      <video id="video-preview" autoPlay></video>
-      <button onClick={manuallyStartCamera}>Start Camera</button>
+      <div style={{ position: 'relative', maxWidth: '640px', margin: 'auto' }}>
+        <video ref={videoRef} id="video-preview" autoPlay style={{ transform: mirror ? 'scaleX(-1)' : 'none' }} />
+        <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <button onClick={takeSelfie}>Take Selfie</button>
+          <button onClick={toggleMirror}>{mirror ? 'Disable Mirror' : 'Enable Mirror'}</button>
+        </div>
+      </div>
+      {/* Display the captured selfie */}
+      {selfieImage && <img src={selfieImage} alt="Selfie" />}
     </div>
   );
 }
 
-export default App
+export default App;
